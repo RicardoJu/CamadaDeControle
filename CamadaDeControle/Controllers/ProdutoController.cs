ba4fda5d0e16e3d9eb0cb717a01161d9542b6d27
RicardoJu <juniorkenobi@gmail.com>
2016-06-19 12:34:30 -0300

    camada de controle

---
+++ CamadaDeControle/Controllers/ProdutoController.cs
@@ -0,0 +1,39 @@
+﻿using CamadaDeControle.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CamadaDeControle.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+		public ActionResult Lista()
+		{
+			K19Context ctx = new K19Context();
+			return View(ctx.Produtos);
+		}
+
+		[HttpGet]
+		public ActionResult Cadastra()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Cadastra(Produto p)
+		{
+			
+
+			K19Context ctx = new K19Context();
+			ctx.Produtos.Add(p);
+			ctx.SaveChanges();
+
+			TempData["Mensagem"]="Produto cadastrado com sucesso!";
+
+			return RedirectToAction("Lista");
+        }
+    }
+}
---
+++ CamadaDeControle/Models/K19Context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CamadaDeControle.Models
+{
+	public class K19Context : DbContext
+
+	{
+		public DbSet<Produto> Produtos { get; set; }
+	}
+}
